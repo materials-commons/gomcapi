00df3c8191839eadd5e1b4b8242910d76e0954bc
gtarcea <glenn.tarcea@gmail.com>
2018-02-18 15:57:38 -0500

    Add CreateProject and DeleteProject as well as tests

--- project.go
+++ project.go
@@ -2,40 +2,55 @@ package mcapi
 
 import (
 	"crypto/tls"
-	"fmt"
 
 	"github.com/materials-commons/config"
 	"gopkg.in/resty.v1"
 )
 
 type Project struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name"`
-	Owner     string    `json:"owner"`
-	Birthtime Timestamp `json:"birthtime"`
-	MTime     Timestamp `json:"mtime"`
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Owner       string    `json:"owner"`
+	Description string    `json:"description"`
+	Birthtime   Timestamp `json:"birthtime"`
+	MTime       Timestamp `json:"mtime"`
 }
 
+// projectsRoute creates the default route path for projects REST API
 func projectsRoute() string {
-	fmt.Println(config.GetString("mcurl"))
 	p := config.GetString("mcurl") + "/v2" + "/projects"
 	return p
 }
 
 var tlsConfig = tls.Config{InsecureSkipVerify: true}
 
-func p() *resty.Request {
+// r is similar to resty.R() except that it sets the TLS configuration and the apikey
+func r() *resty.Request {
 	return resty.SetTLSClientConfig(&tlsConfig).R().SetQueryParam("apikey", config.GetString("apikey"))
 }
 
+// GetAllProjects gets all projects that the user has access to
 func GetAllProjects() ([]*Project, error) {
 	var results []*Project
-	_, err := p().SetResult(&results).Get(projectsRoute())
-	if err != nil {
-		return nil, err
-	}
+	_, err := r().SetResult(&results).Get(projectsRoute())
+	return results, err
+}
 
-	return results, nil
+// GetProject retrieves the given from with projectID
+func GetProject(projectID string) (*Project, error) {
+	var proj Project
+
+	_, err := r().SetResult(&proj).Get(projectsRoute() + "/" + projectID)
+	return &proj, err
+}
+
+// CreateProject creates a new project with the user set as the owner
+func CreateProject(name, description string) (*Project, error) {
+	var proj Project
+	_, err := r().SetResult(&proj).
+		SetBody(map[string]interface{}{"name": name, "description": description}).
+		Post(projectsRoute())
+	return &proj, err
 }
 
 func (p *Project) Update() error {
@@ -44,4 +59,9 @@ func (p *Project) Update() error {
 
 func (p *Project) Delete() error {
 	return nil
+}
+
+func DeleteProject(projectID string) error {
+	_, err := r().Delete(projectsRoute() + "/" + projectID)
+	return err
 }
--- project_test.go
+++ project_test.go
@@ -1,8 +1,6 @@
 package mcapi_test
 
 import (
-	"fmt"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -10,12 +8,32 @@ import (
 )
 
 var _ = Describe("Project", func() {
-	It("Should get all projects", func() {
-		projects, err := GetAllProjects()
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(projects).ShouldNot(BeNil())
-		for _, proj := range projects {
-			fmt.Printf("%+v\n", proj)
-		}
+	var createdProjectID string
+
+	Describe("GetAllProjects", func() {
+		It("Should get all projects for user", func() {
+			projects, err := GetAllProjects()
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(projects).ShouldNot(BeNil())
+		})
+	})
+
+	Describe("CreateProject", func() {
+		It("Should create a project", func() {
+			projName := "Proj1"
+			projDescription := "Project Created With Test"
+			proj, err := CreateProject(projName, projDescription)
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(proj).ShouldNot(BeNil())
+			Expect(proj.Name).Should(Equal(projName))
+			Expect(proj.Description).Should(Equal(projDescription))
+
+			createdProjectID = proj.ID
+		})
+
+		It("Should delete the created project", func() {
+			err := DeleteProject(createdProjectID)
+			Expect(err).Should(BeNil())
+		})
 	})
 })
