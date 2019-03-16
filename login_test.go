cd15843cca0eb1a151e048c5be773d0c9269817e
gtarcea <glenn.tarcea@gmail.com>
2019-03-16 14:43:17 -0400

    Add test for login

---
+++ login_test.go
@@ -0,0 +1,13 @@
+package mcapi
+
+import (
+	"testing"
+
+	"github.com/materials-commons/gomcapi/pkg/tutils/assert"
+)
+
+func TestLogin(t *testing.T) {
+	c, err := Login("test@test.mc", "test", "http://mcdev.localhost/api")
+	assert.Okf(t, err, "Login failed with err :%s", err)
+	assert.NotNil(t, c)
+}
