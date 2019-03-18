82f02b4c7cb76d68db464c157cafe5f2702f4bd4
gtarcea <glenn.tarcea@gmail.com>
2019-03-18 10:43:57 -0400

    Switch Login to use post

--- login.go
+++ login.go
@@ -1,25 +1,21 @@
 package mcapi
 
-import "github.com/materials-commons/gomcapi/pkg/urlpath"
-
 func (c *Client) Login(userID, password string) error {
 	body := map[string]interface{}{
 		"user_id":  userID,
 		"password": password,
 	}
 
-	var res struct {
+	var result struct {
 		Data struct {
 			APIKey string `json:"apikey"`
 		} `json:"data"`
 	}
 
-	resp, err := r().SetResult(&res).SetBody(body).Post(urlpath.Join(c.BaseURL, "login"))
-
-	if err := c.getAPIError(resp, err); err != nil {
+	if err := c.post(&result, body, "login"); err != nil {
 		return err
 	}
 
-	c.APIKey = res.Data.APIKey
+	c.APIKey = result.Data.APIKey
 	return nil
 }
