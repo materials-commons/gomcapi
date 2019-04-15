c9b35dba83ff26bd194662269655344be5c27ddd
gtarcea <glenn.tarcea@gmail.com>
2019-04-15 13:43:11 -0400

    New API call addSampleAndFilesToProcess

--- samples.go
+++ samples.go
@@ -63,6 +63,53 @@ func (c *Client) AddSampleToProcess(projectID, experimentID string, connect Conn
 	return &result.Data, nil
 }
 
+type ConnectSampleAndFilesToProcess struct {
+	ProcessID     string
+	SampleID      string
+	PropertySetID string
+	Transform     bool
+	FilesByName   []string
+	FilesByID     []string
+}
+
+func (c *Client) AddSampleAndFilesToProcess(projectID, experimentID string, connect ConnectSampleAndFilesToProcess) (*Sample, error) {
+	var result struct {
+		Data Sample `json:"data"`
+	}
+
+	body := struct {
+		ProjectID     string   `json:"project_id"`
+		ExperimentID  string   `json:"experiment_id"`
+		ProcessID     string   `json:"process_id"`
+		SampleID      string   `json:"sample_id"`
+		PropertySetID string   `json:"property_set_id"`
+		Transform     bool     `json:"transform"`
+		FilesByName   []string `json:"files_by_name,omitempty"`
+		FilesByID     []string `json:"files_by_id,omitempty"`
+	}{
+		ProjectID:     projectID,
+		ExperimentID:  experimentID,
+		ProcessID:     connect.ProcessID,
+		SampleID:      connect.SampleID,
+		PropertySetID: connect.PropertySetID,
+		Transform:     connect.Transform,
+	}
+
+	if len(connect.FilesByName) != 0 {
+		body.FilesByName = connect.FilesByName
+	}
+
+	if len(connect.FilesByID) != 0 {
+		body.FilesByID = connect.FilesByID
+	}
+
+	if err := c.post(&result, body, "addSampleAndFilesToProcess"); err != nil {
+		return nil, err
+	}
+
+	return &result.Data, nil
+}
+
 type SampleProperty struct {
 	Name         string        `json:"name"`
 	ID           string        `json:"id,omitempty"`
