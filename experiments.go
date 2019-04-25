f99803f1e48305fc15b53bb62ce60a1807093c8a
gtarcea <glenn.tarcea@gmail.com>
2019-04-25 14:00:57 -0400

    Support setting experiment loading indicator

--- experiments.go
+++ experiments.go
@@ -1,6 +1,6 @@
 package mcapi
 
-func (c *Client) CreateExperiment(projectID, name, description string) (*Experiment, error) {
+func (c *Client) CreateExperiment(projectID, name, description string, inProgress bool) (*Experiment, error) {
 	var result struct {
 		Data Experiment `json:"data"`
 	}
@@ -9,6 +9,7 @@ func (c *Client) CreateExperiment(projectID, name, description string) (*Experim
 		"project_id":  projectID,
 		"name":        name,
 		"description": description,
+		"in_progress": inProgress,
 	}
 
 	if err := c.post(&result, body, "createExperimentInProject"); err != nil {
@@ -16,4 +17,20 @@ func (c *Client) CreateExperiment(projectID, name, description string) (*Experim
 	}
 
 	return &result.Data, nil
+}
+
+func (c *Client) UpdateExperimentProgressStatus(projectID, experimentID string, inProgress bool) error {
+	var result struct {
+		Data struct {
+			Success bool `json:"success"`
+		} `json:"data"`
+	}
+
+	body := map[string]interface{}{
+		"project_id":    projectID,
+		"experiment_id": experimentID,
+		"in_progress":   inProgress,
+	}
+
+	return c.post(&result, body, "updateExperimentProgressStatus")
 }
